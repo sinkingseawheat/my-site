8a3748c33e3065b45298ab9a55a74a664e556203
sinking_sea_wheat <52875009+SinkingSeaWheat@users.noreply.github.com>
2025-03-02 17:40:34 +0900

    console.logの削除とtitleプロパティの書き方に冗長性を持たせてた

--- src/app/api/getPageList.ts/route.ts
+++ src/app/api/getPageList.ts/route.ts
@@ -13,7 +13,7 @@ const searchPage = async (dirents:Dirent[], results:Results):Promise<void>=>{
       const absolutePath = path.join(dirent.parentPath, dirent.name)
       const url = absolutePath.replace(process.cwd(), '').replace(/\\/g, "/").replace(/^\/src\/app\/[^\/]+/,'').replace(/page\.tsx$/,'')
       const content = await fs.readFile(absolutePath, 'utf-8')
-      const titleMatched = content.match(/title:`([^`]+)`/);
+      const titleMatched = content.match(/title:\s*`([^`]+)`/);
       const title = titleMatched === null ? `無し` : titleMatched[1];
       const label = (()=>{
         if(/^\/tool\//.test(url)){
@@ -32,6 +32,5 @@ export async function GET() {
   const dirents = await fs.readdir(rootPath, {withFileTypes:true});
   const results:Results = [];
   await searchPage(dirents, results);
-  console.log(results)
   return Response.json(results)
 }
