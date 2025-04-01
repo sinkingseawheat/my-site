54baa1eb5014787a27e16f6caf8e5828cd7da1a7
sinking_sea_wheat <52875009+SinkingSeaWheat@users.noreply.github.com>
2025-04-01 09:11:36 +0900

    TOPページの微修正

--- src/app/(defaultLayout)/page.tsx
+++ src/app/(defaultLayout)/page.tsx
@@ -4,7 +4,7 @@ import {S, LinkText, PageList} from '@components/all'
 
 export const metadata: Metadata = {
   title:`TOPページ`,
-  description:`TOPページです`,
+  description:`Sinking Sea WheatサイトのTOPページです。作成したツールやブログを執筆しています。`,
   alternates: {
     canonical:'/'
   }
@@ -17,7 +17,7 @@ export default async function Page() {
   <article>
     <S.lv2 title="このサイトについて">
       <p>
-        読みづらいのでアカウント名を変更中。ドメインも変更しましたsinkingseawheat.com→midori-no-kiga.com。8月ごろには旧ドメインを失効させる予定です。<br/>
+        読みづらいのでアカウント名を変更中。ドメインも変更しましたsinkingseawheat.com→midori-no-kiga.com。2025年8月ごろには旧ドメインを失効させる予定です。<br/>
         sinkingseawheat→<strong><ruby>緑ノ企鵝<rp>(</rp><rt>ミドリノキガ</rt><rp>)</rp></ruby></strong><br/>
         「企鵝」は中国語でペンギンという意味らしいです。「人鳥」よりも字面がかっこいいと感じたので採用しました。
       </p>
@@ -26,11 +26,11 @@ export default async function Page() {
       </p>
       <p>
         作成したツールなどを公開しています。<br />
-        ブログで製品・サービスレビューとかも書いています（アフェリエイトリンクあり）。
+        ブログも書いています。<LinkText href="/blog/" isOpenAnotherTab={false} elm={`緑ノ企鵝(ミドリノキガ)blog`} prefetch={false} />
       </p>
       <p>
         このサイトの/blog配下を除いて、Next.jsのSSG生成で作成しています。<br />
-        <LinkText href='https://github.com/sinkingseawheat/my-site' isOpenAnotherTab={true} elm={`このサイトのビルド前ソース（GitHubのページへ移動します）`}/>を公開しています。<br />
+        <LinkText href='https://github.com/sinkingseawheat/my-site' isOpenAnotherTab={true} elm={`このサイトのビルド前ソース（GitHubのページへ移動します）`} />を公開しています。<br />
       </p>
     </S.lv2>
     <PageList />
