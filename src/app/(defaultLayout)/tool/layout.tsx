703565eb4a53e728c42ff11f1bc0bfcfe0aa3913
midori-no-kiga <52875009+sinkingseawheat@users.noreply.github.com>
2025-04-12 08:00:09 +0900

    titleのデフォルト値の修正

--- src/app/(defaultLayout)/tool/layout.tsx
+++ src/app/(defaultLayout)/tool/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title:{
-    default:`sinkingseawheatのページです`,
+    default:`Sinking Sea Wheatのtoolページ`,
     template:`%s | tool`
   },
 };
