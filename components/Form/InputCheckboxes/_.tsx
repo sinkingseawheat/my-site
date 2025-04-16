7e4ee3f1aae2aa0050d8cd7b5f9c846ffff11d11
midori-no-kiga <52875009+sinkingseawheat@users.noreply.github.com>
2025-04-16 09:01:04 +0900

    InputTextと型に統一性を持たせる

--- components/Form/InputCheckboxes/_.tsx
+++ components/Form/InputCheckboxes/_.tsx
@@ -1,29 +1,23 @@
 'use client'
-import { type ReactNode } from 'react'
 import style from './_.module.css'
-import { type UseFormRegisterReturn } from "react-hook-form"
+import { type FormInputItemExtended } from '@components/utility'
 
 export default function InputCheckBoxes({
   elms, message,
-}:{
-  elms:{
-    label: ReactNode,
-    registerReturn: UseFormRegisterReturn,
-  }[],
-  message?: string,
-}){
+}:FormInputItemExtended){
 
   return  (
     <div className={`${style.wrap}`}>
       <div className={style.wrapCheckboxes}>
         {elms.map(({
           label,
-          registerReturn
+          registerReturn,
+          inputHTMLAttribute,
         })=>{
           return(
             <div className={style.wrapCheckboxes__item} key={registerReturn.name}>
               <label className={style.label}>
-                <input className={style.input} type='checkbox' {...registerReturn}/>
+                <input className={style.input} type='checkbox' {...registerReturn} {...inputHTMLAttribute}/>
                 <span className={style.pseudo} role='presentation'>
                 <svg className={style.svg} viewBox="0 0 21 16" xmlns="http://www.w3.org/2000/svg">
                   <use href='#svgFormCheckmark' />
