023eef9b67d00e258355ebc7a3c7f69987daa7da
midori-no-kiga <52875009+sinkingseawheat@users.noreply.github.com>
2025-04-19 09:47:33 +0900

    CSS変数の定義を簡素化

--- components/SVGIcon/_.tsx
+++ components/SVGIcon/_.tsx
@@ -1,6 +1,4 @@
-export default function SVGIcon(){
-  return (
-    <div style={{display:'none'}}>
+import { StyleValue } from "@components/utility"
       <svg id="svgFormCheckmark" width="21" height="16" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M6.30559 14.7197C6.88308 15.2629 7.78358 15.2629 8.36108 14.7197L19.5682 4.17769C20.1979 3.58539 20.1979 2.58483 19.5682 1.99252L18.8111 1.2803C18.2336 0.737084 17.3331 0.737084 16.7556 1.2803L7.67591 9.8211C7.48342 10.0022 7.18325 10.0022 6.99075 9.82109L4.24441 7.23775C3.66692 6.69453 2.76642 6.69453 2.18893 7.23775L1.43177 7.94997C0.802091 8.54227 0.802092 9.54284 1.43177 10.1351L6.30559 14.7197Z" fill="rgb(var(--color-bg))" stroke="rgb(var(--color-fg))" />
       </svg>
--- components/utility.ts
+++ components/utility.ts
@@ -1,11 +1,13 @@
 import { type ReactNode, type InputHTMLAttributes, TextareaHTMLAttributes } from "react"
 import { type UseFormRegisterReturn } from "react-hook-form"
 
-export type VariableCSSProperties<TKeys extends `--${string}`[]> = React.CSSProperties & {
+/* export type VariableCSSProperties<TKeys extends `--${string}`[]> = React.CSSProperties & {
   [key in TKeys[number]]:string
 }
 
-export type SetCSSVariable<TKeys extends `--${string}`[]> = (styleValue:Partial<Record<string,string>>)=>VariableCSSProperties<TKeys>
+export type SetCSSVariable<TKeys extends `--${string}`[]> = (styleValue:Partial<Record<string,string>>)=>VariableCSSProperties<TKeys> */
+
+export type StyleValue<T extends `--${string}`> = React.CSSProperties & Partial<Record<T,string>>
 
 export type FormInputItemExtended<TIsArray extends Boolean = true> = {
   elms: TIsArray extends true ? {
