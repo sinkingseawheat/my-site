a8e79d7fe66bef5e32dacd6f7531b2e5d2565d9d
midori-no-kiga <52875009+sinkingseawheat@users.noreply.github.com>
2025-04-26 15:08:10 +0900

    追加のクラスを親から受け取れるようにした

--- components/Form/Button/_.tsx
+++ components/Form/Button/_.tsx
@@ -8,8 +8,9 @@ type ButtonElm = {
 export default function Button({
   children, onClick, type, isDisabled, className
 }:ButtonElm){
+  const mergedClassName = className ? `${style.button} ${className}` : style.button
     return (
-    <button className={style.button} type={type ?? 'submit'} onClick={onClick} disabled={isDisabled}>
+    <button className={mergedClassName} type={type ?? 'submit'} onClick={onClick} disabled={isDisabled}>
       {
         typeof children === 'string' ?
         <span className={style.button_i}>{children}</span>
