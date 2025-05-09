7374c55f97fd20db8a18be1848b268517070c817
midori-no-kiga <52875009+sinkingseawheat@users.noreply.github.com>
2025-05-09 12:52:21 +0900

    対象ファイルを絞り込む引数filterを追加

--- preprocess/explorerFilesRecursively.ts
+++ preprocess/explorerFilesRecursively.ts
@@ -1,6 +1,6 @@
 import fs from 'fs/promises'
 import path from 'path'
-import { type Stats } from 'fs'
+import { Dirent, type Stats } from 'fs'
 
 export type Survey = (
     fullPath:string,
@@ -10,18 +10,14 @@ export type Survey = (
 )  => Promise<unknown> | unknown
 
 /**
- * Resultを絶対パスをキーにしてまとめた情報
- */
-type Results = Map<string, unknown>
-
-/**
  * 指定されたディレクトリ以下のファイルを再帰的に探索し、条件に合致するファイルを処理します。
  * params.surveyの返却結果はunknownとしていますので、zodなどでスキーマバリデーションを行ってください。
  *
  * @param {object} params - パラメータオブジェクト
  * @param {string} params.dirPath - 探索を開始するディレクトリのパス。
  * @param {Survey[]} params.surveys - 探索するファイルに対する調査内容の配列。
- * @param {Results[]} [params._results=[]] - 内部で使用するparams.surveyの返却結果の配列。外部から渡す必要はありません。
+ * @param {(fullPath:string, entry:Dirent)=>boolean} params.filter - ファイルに対するフィルター。省略時は全てを対象にする。
+ * @param {Map<string, unknown>[]} [params._results=[]] - 内部で使用するparams.surveyの返却結果の配列。外部から渡す必要はありません。
  * @returns {Promise<unknown | null>} - 条件に合致したファイルの情報を含む結果の配列をPromiseで返します。
  * 探索中にエラーが発生した場合や、条件に合致するファイルが一つもなかった場合は `null` を返します。
  *
@@ -30,18 +26,22 @@ type Results = Map<string, unknown>
 export async function explorerFilesRecursively(
   dirPath: string,
   surveys: Survey[],
-  _results?: Results[]
-): Promise<Results[]|null> {
+  filter?: (fullPath:string, entry:Dirent)=>boolean,
+  _results?: Map<string, unknown>[]
+): Promise<Map<string, unknown>[]|[]> {
   // 絶対パスのみを受け入れる
-  if(!path.isAbsolute(dirPath)) return null
-  const results:Exclude<Results, undefined>[] = _results === undefined ? surveys.map(()=>new Map()) : _results
+  if(!path.isAbsolute(dirPath)) return []
+  const results:Exclude<Map<string, unknown>, undefined>[] = _results === undefined ? surveys.map(()=>new Map()) : _results
   try {
     const entries = await fs.readdir(dirPath, { withFileTypes: true })
     for (const entry of entries) {
       const fullPath = path.join(dirPath, entry.name)
       if (entry.isDirectory()) {
-        await explorerFilesRecursively(fullPath, surveys, results)
-      } else if (entry.isFile() && entry.name === 'page.tsx') {
+        await explorerFilesRecursively(fullPath, surveys, filter, results)
+      } else if (
+        entry.isFile()
+        && (filter!==undefined ? filter(fullPath, entry) : true)
+      ) {
         try {
           const url = fullPath.replace(process.cwd(), '').replace(/\\/g, "/").replace(/^\/src\/app\/[^\/]+/,'').replace(/page\.tsx$/,'')
           const [content, stat] = await Promise.all([
@@ -50,19 +50,19 @@ export async function explorerFilesRecursively(
           ])
           await Promise.all(
             surveys.map(async (survey, index)=>{
-              const result = await survey(fullPath, stat, url, content)
+              const result:Awaited<ReturnType<Survey>> = await survey(fullPath, stat, url, content)
               results[index].set(fullPath, result)
             })
           )
         } catch (error) {
           console.error(`Error processing file: ${fullPath}`, error)
-          return null
+          return []
         }
       }
     }
     return results
   } catch (error) {
     console.error(`Error reading directory: ${dirPath}`, error)
-    return null
+    return []
   }
 }
