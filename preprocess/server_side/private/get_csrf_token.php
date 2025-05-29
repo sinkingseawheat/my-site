9413de73737e2938497e04de7900bdcff85d9d77
midori-no-kiga <52875009+sinkingseawheat@users.noreply.github.com>
2025-05-29 09:46:25 +0900

    requireして利用する関数に変更

--- preprocess/server_side/private/get_csrf_token.php
+++ preprocess/server_side/private/get_csrf_token.php
@@ -1,20 +1,9 @@
 <?php
-if(session_status() === PHP_SESSION_NONE){
-  session_start();
-}
 
-// secret.phpの読み込み
-// require_once dirname(__FILE__, 3) . '/server_action_private/secret.php';
-
-function generateCSRFToken() {
+function generateCSRFToken($name_client) {
   $token = bin2hex(random_bytes(32));
-  $_SESSION['csrf_token'] = $token;
+  $_SESSION[$name_client] = $token;
   return $token;
 }
-
-$csrf_token = generateCSRFToken();
-
-header('Content-Type: application/json');
-echo json_encode(['csrf_token' => $csrf_token]); // hex値なのでJSON_UNESCAPED_UNICODEは不要
 
 ?>
