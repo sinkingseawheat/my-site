9f78f52e9a0b433c6a982c5ee9e308f5e6aff8cd
midori-no-kiga <52875009+sinkingseawheat@users.noreply.github.com>
2025-05-29 15:40:39 +0900

    数字をカンマ区切りにするユーティリティ関数を追加

--- components/utility.ts
+++ components/utility.ts
@@ -26,3 +26,25 @@ export type FormTextareaItemExtended = {
 }
 
 export const FOCUSABLE_ELEMENTS_SELECTOR = 'a[href],button,[tabindex]:not([tabindex="-1"]),[role="button"],input:not([type="hidden"]),textarea,select,iframe'
+
+export const getNumbersWithDigitSeparators = (targetNumbers:number|string, separator:string=','):string => {
+  let returnValue:string = ''
+  if(typeof targetNumbers === 'string'){
+    if(
+      !/^[\-\+]?(0|[1-9]\d*)$/.test(targetNumbers) // 0以外の数値で、0始まりの場合
+      || !(new RegExp(`^[\\-\\+]?[\\d\\${separator}]`).test(targetNumbers)) // 数値とセパレータ以外を含む
+    ){
+      console.error(`「${targetNumbers}」を桁区切りにできませんでした`)
+      return targetNumbers // 桁区切りにできなかったらそのまま返す
+    }
+  }else{
+    if(
+      Math.abs(targetNumbers) !== Math.abs(Math.floor(targetNumbers))
+    ){
+      console.error(`「${targetNumbers}」を桁区切りにできませんでした`)
+      return targetNumbers.toString() // 桁区切りにできなかったらstring型に変換するだけ
+    }
+  }
+  returnValue = typeof targetNumbers === 'number' ? targetNumbers.toString() : targetNumbers.replaceAll(separator, '')
+  return returnValue.replace(/(\d+?)(?=(\d{3})+(?!\d))/g, `$1${separator}`)
+}
