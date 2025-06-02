58b4450782fe5db5c74e2b40dffaa27537275b51
midori-no-kiga <52875009+sinkingseawheat@users.noreply.github.com>
2025-06-02 14:28:20 +0900

    L.gridは入れ子になる可能性があり、親のstyleValueが子に影響が与える場合がある。その影響を無効化する

--- components/Layouts/grid/_.tsx
+++ components/Layouts/grid/_.tsx
@@ -11,11 +11,22 @@ export default function grid({
   styleValue,
 }:{
   children?: React.ReactNode,
-} & {styleValue?: StyleValue<'--min-width'|'--column-gap'|'--row-gap'|'--margin-top'|'--fill-or-fit'>})
+} & {styleValue?: StyleValue<'--min-width'|'--max-width'|'--column-gap'|'--row-gap'|'--margin-top'|'--fill-or-fit'>})
 {
+  const _styleValue = {
+    ...{
+      '--min-width':'auto',
+      '--max-width':'1fr',
+      '--column-gap':'1rem',
+      '--row-gap':'1rem',
+      '--margin-top':'0',
+      '--fill-or-fit':'auto-fit',
+    },
+    ...styleValue
+  }
   if(children === undefined || children === null){return <></>}
   return (
-    <div className={style.wrap} style={styleValue}>
+    <div className={style.wrap} style={_styleValue}>
       {
         React.Children.map(
           children, (item, index)=>
