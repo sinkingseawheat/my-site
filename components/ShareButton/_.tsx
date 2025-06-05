4508e962b8a24d39e9e520b9a110651259f98d3e
midori-no-kiga <52875009+sinkingseawheat@users.noreply.github.com>
2025-06-05 16:34:27 +0900

    iPadのSafariでXのシェアぼたんが効かなかったり、Lineのシェアボタンの挙動が怪しかったりするので、シンプルにページのURLとタイトルをコピーする方法に変更

--- components/ShareButton/_.tsx
+++ components/ShareButton/_.tsx
@@ -1,7 +1,8 @@
 'use client'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, use } from 'react';
 import { usePathname, useSearchParams  } from 'next/navigation';
-import { Section } from '@components/all';
+import { Section, L, F, SVGIcon } from '@components/all';
+import { PopupContext } from '@components/context';
 
 import style from './_.module.css';
 
@@ -9,68 +10,39 @@ export default function ShareButton(){
 
   const [currentHref, setCurrentHref] = useState<null | string>(null)
   const [pageTitle, setPageTitle] = useState<null | string>(null)
+  const [, setPopupMessage] = use(PopupContext)
 
   const _pathname = usePathname()
   const _searchParams = useSearchParams()
 
   useEffect(()=>{
-    setCurrentHref(encodeURIComponent(window.location.href));
-    setPageTitle(encodeURIComponent(document?.getElementsByTagName('title')?.[0]?.textContent ?? ''))
+    setCurrentHref(window.location.href);
+    setPageTitle(document?.getElementsByTagName('title')?.[0]?.textContent ?? '')
   },[_pathname, _searchParams])
 
   return currentHref !== null && (
     <Section type='dl' title="記事を共有する">
-      <ul className={style.wrap}>
-            <li className={style.item}>
-              <a
-                className={`${style.link} ${style['-x']} twitter-share-button`}
-                href='https://twitter.com/share?ref_src=twsrc%5Etfw'
-                data-show-count='false'
-                target='_blank'
-              >
-                <img
-                  className={style.icon}
-                  width='1200'
-                  height='1227'
-                  src='/c/vendor/logo/logo_x.svg'
-                  alt='X'
-                />
-              </a>
-            </li>
-            <li className={style.item}>
-              <a
-                className={style.link}
-                href={currentHref === null ? `javascript:void(0);` : `https://bsky.app/intent/compose?text=${currentHref}`}
-                target='_blank'
-              >
-                <img
-                  className={style.icon}
-                  width='576'
-                  height='512'
-                  src='/c/vendor/logo/bluesky.svg'
-                  alt='bluesky'
-                />
-              </a>
-            </li>
-            <li className={style.item}>
-              <a
-                className={style.link}
-                href={currentHref === null ? `javascript:void(0);` : `https://social-plugins.line.me/lineit/share?url=${currentHref}`}
-                target='_blank'
-              >
-                <img className={style.icon} src={`/c/vendor/logo/LINE_Brand_icon.png`} alt='Line' />
-              </a>
-            </li>
-            <li className={style.item}>
-              <a
-                className={style.link}
-                href={currentHref === null ? `javascript:void(0);` : `http://b.hatena.ne.jp/add?mode=confirm&url=${currentHref}&title=${pageTitle}`}
-                target='_blank'
-              >
-                <img className={style.icon} src={`/c/vendor/logo/hatenabookmark_symbolmark.png`} alt='はてなブックマーク'/>
-              </a>
-            </li>
-          </ul>
+      <hr />
+      <p>ボタンを押すとページのURLとタイトルがコピーされます。</p>
+      <L.flex>
+        <p className={style.source}>
+          {currentHref ?? ''}<br />{pageTitle ?? ''}
+        </p>
+        <F.Button
+          aria-label='ページのURLとタイトルをコピーする'
+          onClick={async ()=>{
+            if(currentHref){
+              await navigator.clipboard.writeText(currentHref + (pageTitle ? '\n' + pageTitle : ''))
+              setPopupMessage(<>{currentHref ?? ''}<br />{pageTitle ?? ''}</>)
+            }else{
+              setPopupMessage('ページURLの取得に失敗しました')
+            }
+          }}
+        >
+          <SVGIcon.copy styleValue={{'--color-stroke':'var(--color-bg)'}} />
+        </F.Button>
+      </L.flex>
+      <hr />
     </Section>
   );
 }
